7c039deef017f88714bb3dd07285f444c822ebf4
AdoraWyne <adora.kong@envato.com>
2026-01-02 23:30:24 +1100

    Apply the function to App.tsx - the main page for now.
    - Add max length: 100
    - No validations so far

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,27 @@
+import { useState } from "react";
+import { formatKebabToTitle } from "./utils/formatKebabToTitle";
+import "./App.css";
+
 function App() {
+  const [value, setValue] = useState<string>("adora-hou-van-appel");
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) =>
+    setValue(e.target.value);
+
+  const transformedKebabToTitle = formatKebabToTitle(value);
+
   return (
-    <>
-      <div>testing</div>
-      <div>count</div>
-    </>
+    <div>
+      <label htmlFor="kebab-input">Enter Kebab case here: </label>
+      <input
+        id="kebab-input"
+        type="text"
+        maxlength="100"
+        value={value}
+        onChange={handleChange}
+      />
+      <div>Kebab Transformer ✨: {transformedKebabToTitle}</div>
+    </div>
   );
 }
 
