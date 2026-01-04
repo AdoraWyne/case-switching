0fdc1ed2ad3a97aa0230a3b0da57496594b42352
AdoraWyne <adora.kong@envato.com>
2026-01-04 21:52:22 +1100

    Turn it to a form and use `formData` (uncontrolled form)

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,40 @@
-import { useRef, useState } from "react";
+import { useState } from "react";
 import { formatKebabToTitle } from "./utils/formatKebabToTitle";
 import "./App.css";
 
 function App() {
-  const inputRef = useRef<HTMLInputElement>(null);
   const [transformedKebabToTitle, setTransformedKebabToTitle] =
     useState<string>("");
 
-  const handleClick = () => {
-    setTransformedKebabToTitle(formatKebabToTitle(inputRef.current?.value));
+  const handleSubmit = (e: HTMLFormElement) => {
+    e.preventDefault();
+
+    const formData = new FormData(e.currentTarget);
+    const kebabInput = formData.get("kebab-input");
+    setTransformedKebabToTitle(formatKebabToTitle(kebabInput));
   };
 
   return (
     <div>
-      <label htmlFor="kebab-input">Enter Kebab case here: </label>
-      <input
-        id="kebab-input"
-        type="text"
-        name="kebab-input"
-        maxLength="100"
-        ref={inputRef}
-      />
-      <button type="button" onClick={handleClick}>
-        Abadacabara ✨!
-      </button>
-      <br />
-      <label htmlFor="kebab-output">Kebab Transformer ✨: </label>
-      <output id="kebab-output" name="kebab-output" htmlFor="kebab-input">
-        {transformedKebabToTitle}
-      </output>
+      <h1>Kebab case to Title case Transformer</h1>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="kebab-input">Enter Kebab case here: </label>
+        <br />
+        <input
+          id="kebab-input"
+          type="text"
+          name="kebab-input"
+          maxLength="100"
+        />
+        <br />
+        <button type="submit">Abadacabara ✨!</button>
+        <br />
+        <label htmlFor="kebab-output">Kebab Transformer ✨: </label>
+        <br />
+        <output id="kebab-output" name="kebab-output" htmlFor="kebab-input">
+          {transformedKebabToTitle}
+        </output>
+      </form>
     </div>
   );
 }
